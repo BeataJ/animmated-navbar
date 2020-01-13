e07fe04edc66c920654d57b88dbeb657ee7d8607
beata <beata@emc2wave.com>
2020-01-13 14:42:35 -0800

    add target.className

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ let observer = new IntersectionObserver(navCheck, options);
 
 function navCheck(entries) {
   entries.forEach(entry => {
-    console.log(entry);
+    const className = entry.target.className;
+    console.log(className);
   });
 }
 
