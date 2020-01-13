b7fafac9649fbece8124e8caeee7815bb52afa27
beata <beata@emc2wave.com>
2020-01-13 13:43:25 -0800

    add varables sections, bubbel, gradients

---
+++ app.js
@@ -0,0 +1,7 @@
+const sections = document.querySelectorAll("section");
+const bubbel = document.querySelector(".bubbel");
+const gradients = [
+  "linear-gradient(to right, #ffb88c, #de6262)",
+  "linear-gradient(to right, #ffb88c, #de6262)",
+  "linear-gradient(to right, #cbad6d, #d53369)"
+];
