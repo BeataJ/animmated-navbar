823aab7e8bec206f1dcfbb83ec1a443fe00337fb
beata <beata@emc2wave.com>
2020-01-13 13:51:28 -0800

    add consts opstions, observer

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ const gradients = [
   "linear-gradient(to right, #ffb88c, #de6262)",
   "linear-gradient(to right, #cbad6d, #d53369)"
 ];
+
+const options = {
+  threshold: 0.7
+};
+
+let observer = new IntersectionObserver(navCheck, options);
